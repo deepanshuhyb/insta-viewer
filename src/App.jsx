67dd1b45259451aab1c6501c9573c2d27aecd37b
deepanshuhyb <deepanshuhyb@gmail.com>
2024-09-23 20:11:03 +0530

    ui fix

--- src/App.jsx
+++ src/App.jsx
@@ -33,18 +33,18 @@ const Fetchdata = async (e) => {
   
   return (
     <>
-        <div className='h-16 bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-between px-16 w-screen max-w-full'>
-         <div className='font-bold text-2xl px-16'>
+        <div className={`h-16 bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-between px-16 w-screen max-w-full ${togglemenu ? 'px-2' : 'px-16'}`}>
+         <div className={`font-bold text-2xl px-16 ${togglemenu ? 'px-0' : 'px-2'}`}>
             Instory
           </div>
           <div className='md:hidden' onClick={tap}>
             <img src={burgerIcon} alt="Menu Icon" className="w-6 h-6" />
           </div>
           <ul className={`flex md:flex ${togglemenu ? 'block' : 'hidden'} md:block`}>
-            <li className='px-4'><a href="#">Home</a></li>
-            <li className='px-4'><a href="#">Api</a></li>
-            <li className='px-4'><a href="#">Contact</a></li>
-            <li className='px-4'><a href="#">About</a></li>
+            <li className={`${togglemenu ? 'px-2' : 'px-4'}`}><a href="#">Home</a></li>
+            <li className={`${togglemenu ? 'px-2' : 'px-4'}`}><a href="#">Api</a></li>
+            <li className={`${togglemenu ? 'px-2' : 'px-4'}`}><a href="#">Contact</a></li>
+            <li className={`${togglemenu ? 'px-2' : 'px-4'}`}><a href="#">About</a></li>
           </ul>
         </div>
         <div className='flex flex-col h-dvh bg-gradient-to-r from-violet-500 to-pink-700'>

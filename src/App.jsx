36a9e94cfc66dcadb4e0d56b9ef1376f8936cb4e
deepanshuhyb <deepanshuhyb@gmail.com>
2024-10-06 23:05:59 +0530

    added story div

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import burgerIcon from './assets/burger-bar.png';
 import searchBtn from './assets/search-btn.svg';
 import { getURL } from './api';
 function App() {
   const [togglemenu, settoggal] = useState(false);
+  const [name, setName] = useState('');
+  const [pfp, setPfp] = useState("");
+
+  
   const tap = (()=>{
     settoggal(!togglemenu);
   })
-  const [name, setName] = useState('');
+
+
   const userInput = (e)=>{
+    // e.preventDefault();
     setName(e.target.value);
     // console.log(name);
   }
-const options = {
+
+
+  const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '8033dbe000msheb96a535a1c94b8p1103afjsn84f5ad2a2730',
@@ -20,16 +28,29 @@ const options = {
 	}
 };
 
-const Fetchdata = async (e) => {
-  e.preventDefault();
-  try {
-    const response = await fetch(getURL(name), options);
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
+
+  const Fetchdata = async (e) => {
+    e.preventDefault();
+    setName('');
+    try {
+      const response = await fetch(getURL(name), options);
+      const results = await response.json();
+      console.log(results);
+      setPfp(results.stories.result[0].video_versions[0].url);
+      // setPfp(res)
+      console.log(pfp);
+    } catch (error) {
+      console.error(error);
+    } 
   } 
+  
+const showStories = () =>{
+  
 }
+
+  function createDiv() {
+  }
+
   
   return (
     <>
@@ -50,23 +71,24 @@ const Fetchdata = async (e) => {
         <div className='flex flex-col h-dvh bg-gradient-to-r from-violet-500 to-pink-700'>
             <div className='flex justify-center'>
                 <form>
-                  <label htmlFor=""><p className='flex justify-center font-sans-serif antialiased text-black text-2xl py-2 '>Enter UserName:</p>
-                    <div className='rounded-full h-12 md:w-96 bg-white flex items-center justify-center w-48'>
+                  <label htmlFor=""><p className='flex justify-center font-sans-serif antialiased text-black md:text-2xl py-2'>Enter UserName:</p>
+                    <div className='rounded-full md:h-12 md:w-96 bg-white flex items-center justify-center w-48'>
                       <input className=' h-8 md:w-80 outline-none w-32' type="text" name="" id="" value={name} onChange={function demo(e){
                         return userInput(e);
                       }} />
-                  <button type="submit" ><img src={searchBtn} alt="" className='h-8' onClick={Fetchdata}/></button>
-                  
+                  <button type="submit" onSubmit={Fetchdata} ><img src={searchBtn} alt="" className='md:h-8 h-6' onClick={Fetchdata}/></button>
                     </div>
                   </label>
                 </form>
             </div>
-          <div id='resultArea' className='flex justify-around pt-16'>
-            <button type="button" className=''>Stories</button>
-              <div>
-                      
-              </div>
-            <button type="button" className=''>Profile Picture</button>
+          <div id='resultArea' className='flex flex-col items-center pt-16'>
+            
+              <h1 className=''>Stories</h1>
+                <div className='py-6 '>
+                  <video src={`${pfp}`} className='w-60 h-fit md:w-90' controls>
+
+                  </video>
+                </div>
 
           </div>
         </div>

9074b2dba77778a85007eed7d04739fabc2c5f4e
deepanshu saxena <111433504+deepanshuhyb@users.noreply.github.com>
2024-12-20 16:18:27 +0530

    reverting

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ function App () {
       const response = await fetch(getURL(name), options)
       const results = await response.json()
       console.log(results)
-      if(results.stories.isArray()){
-        setres(results.stories)
+      if(results?.stories.result.isArray()){
+        setres(results.stories.result)
       }else{
         setres([])
       }

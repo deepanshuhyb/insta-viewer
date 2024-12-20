0bafdc0581bd9db7893d70bebd6ff656a4c58fca
deepanshu saxena <111433504+deepanshuhyb@users.noreply.github.com>
2024-12-20 16:06:11 +0530

    key change

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App () {
     headers: {
       // 'x-rapidapi-key': '8033dbe000msheb96a535a1c94b8p1103afjsn84f5ad2a2730',
       // 'x-rapidapi-key': '774e572a4amsh48abd3f8b1ed240p12e268jsn0e7d16756ea5',
-      'x-rapidapi-key': '6b210280c3msh29cc27189c6e5bcp1d3dc7jsn7f93bad2dd1b',
+      'x-rapidapi-key': '774e572a4amsh48abd3f8b1ed240p12e268jsn0e7d16756ea5',
       'x-rapidapi-host': 'instagram-viewer.p.rapidapi.com'
     }
   }
